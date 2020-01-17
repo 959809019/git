00fd3c39841588533a035b35946ba794d5c3c180
lixiuan <959809019@qq.com>
2020-01-17 10:48:23 +0800

    123

---
+++ gittest/src/com/shop/domain/user.java
@@ -0,0 +1,30 @@
+package com.shop.domain;
+
+public class user {
+    private Integer id;
+    private String username;
+
+    @Override
+    public String toString() {
+        return "user{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
